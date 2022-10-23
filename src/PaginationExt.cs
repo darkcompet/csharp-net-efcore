3eeeccd2507c90c1a19601b1d96b5b58b6cc4c17
DarkCompet <darkcompet@gmail.com>
2022-10-23 11:35:04 +0700

    Add padding items

--- src/PaginationExt.cs
+++ src/PaginationExt.cs
@@ -13,10 +13,10 @@ public static class PaginationExt {
 		var offset = Math.Max(0, (pagePos - 1) * pageSize);
 
 		// Use `CountAsync()` since we are using EF Core.
-		// Use `Count()` will lead to weird result.
+		// TechNote: Use `Count()` will lead to weird result !! Don't know why.
 		var totalItemCount = await query.CountAsync();
 
-		// Query and take some items in range [offset + 1, offset + pageSize]
+		// Query and take some items in range [offset, offset + pageSize - 1]
 		var items = await query.Skip(offset).Take(pageSize).ToArrayAsync();
 
 		// This calculation is faster than `Math.Ceiling(rowCount / pageSize)`
@@ -24,6 +24,41 @@ public static class PaginationExt {
 
 		return new PagedResult<T>(
 			items: items,
+			pagePos: pagePos,
+			pageCount: pageCount,
+			totalItemCount: totalItemCount
+		);
+	}
+
+	/// @param pagePos Position of the page. Index from 1 (NOT from 0).
+	/// @param pageSize Page item count. Indicates how many items in each page (for eg,. 10, 20, 50,...).
+	/// Note: given `pageIndex * pageSize` must be in range of Int32.
+	public static async Task<PagedResult<T>> PaginateDk<T>(this IQueryable<T> query, T[] paddingItems, int pagePos = 1, int pageSize = 50) where T : class {
+		// Prevent negative index since maybe zero- or overflow
+		var offset = Math.Max(0, (pagePos - 1) * pageSize);
+
+		// Use `CountAsync()` since we are using EF Core.
+		// TechNote: Use `Count()` will lead to weird result !! Don't know why.
+		var totalItemCount = paddingItems.Length + (await query.CountAsync());
+
+		// Query and take some items in range [offset, offset + pageSize - 1]
+		var items = new List<T>();
+		var remainPaddingCount = paddingItems.Length - offset;
+		if (remainPaddingCount > 0) {
+			var takeCount = Math.Min(remainPaddingCount, pageSize);
+			items.AddRange(paddingItems.Skip(offset).Take(takeCount));
+		}
+		if (remainPaddingCount < pageSize) {
+			var takeCount = pageSize - Math.Max(0, remainPaddingCount);
+			var fromIndex = Math.Max(0, offset - paddingItems.Length);
+			items.AddRange(await query.Skip(fromIndex).Take(takeCount).ToArrayAsync());
+		}
+
+		// This calculation is faster than `Math.Ceiling(rowCount / pageSize)`
+		var pageCount = (totalItemCount + pageSize - 1) / pageSize;
+
+		return new PagedResult<T>(
+			items: items.ToArray(),
 			pagePos: pagePos,
 			pageCount: pageCount,
 			totalItemCount: totalItemCount
