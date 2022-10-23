8434583828a3eef554a895f62709170c0dfa31fc
DarkCompet <darkcompet@gmail.com>
2022-10-23 16:18:50 +0700

    Make pagination with left padding items more clear

--- src/PaginationExt.cs
+++ src/PaginationExt.cs
@@ -5,11 +5,16 @@ using Microsoft.EntityFrameworkCore;
 // Extension of IQueryable<T>
 // Where T: item type
 public static class PaginationExt {
-	/// @param pagePos Position of the page. Index from 1 (NOT from 0).
-	/// @param pageSize Page item count. Indicates how many items in each page (for eg,. 10, 20, 50,...).
 	/// Note: given `pageIndex * pageSize` must be in range of Int32.
-	public static async Task<PagedResult<T>> PaginateDk<T>(this IQueryable<T> query, int pagePos = 1, int pageSize = 50) where T : class {
-		// Maybe zero- or overflow
+	/// @param pagePos: Index (from 1) of the page. For eg,. 1, 2, 3,...
+	/// @param pageSize: Item count in the page. For eg,. 10, 20, 50,...
+	public static async Task<PagedResult<T>> PaginateDk<T>(
+		this IQueryable<T> query,
+		int pagePos,
+		int pageSize
+	) where T : class {
+		// Offset equals to item-count far to now.
+		// Prevent negative index since maybe zero- or overflow
 		var offset = Math.Max(0, (pagePos - 1) * pageSize);
 
 		// Use `CountAsync()` since we are using EF Core.
@@ -30,28 +35,34 @@ public static class PaginationExt {
 		);
 	}
 
-	/// @param pagePos Position of the page. Index from 1 (NOT from 0).
-	/// @param pageSize Page item count. Indicates how many items in each page (for eg,. 10, 20, 50,...).
 	/// Note: given `pageIndex * pageSize` must be in range of Int32.
-	public static async Task<PagedResult<T>> PaginateDk<T>(this IQueryable<T> query, T[] paddingItems, int pagePos = 1, int pageSize = 50) where T : class {
+	/// @param leftPaddingItems: Items which be added at left of query result.
+	/// @param pagePos: Index (from 1) of the page. For eg,. 1, 2, 3,...
+	/// @param pageSize: Item count in the page. For eg,. 10, 20, 50,...
+	public static async Task<PagedResult<T>> PaginateDk<T>(
+		this IQueryable<T> query,
+		T[] leftPaddingItems,
+		int pagePos,
+		int pageSize
+	) where T : class {
+		// Offset equals to item-count far to now.
 		// Prevent negative index since maybe zero- or overflow
 		var offset = Math.Max(0, (pagePos - 1) * pageSize);
 
 		// Use `CountAsync()` since we are using EF Core.
 		// TechNote: Use `Count()` will lead to weird result !! Don't know why.
-		var totalItemCount = paddingItems.Length + (await query.CountAsync());
+		var totalItemCount = leftPaddingItems.Length + (await query.CountAsync());
 
 		// Query and take some items in range [offset, offset + pageSize - 1]
 		var items = new List<T>();
-		var remainPaddingCount = paddingItems.Length - offset;
-		if (remainPaddingCount > 0) {
-			var takeCount = Math.Min(remainPaddingCount, pageSize);
-			items.AddRange(paddingItems.Skip(offset).Take(takeCount));
+		if (offset < leftPaddingItems.Length) {
+			var takeCount = Math.Min(leftPaddingItems.Length - offset, pageSize);
+			items.AddRange(leftPaddingItems.Skip(offset).Take(takeCount));
 		}
-		if (remainPaddingCount < pageSize) {
-			var takeCount = pageSize - Math.Max(0, remainPaddingCount);
-			var fromIndex = Math.Max(0, offset - paddingItems.Length);
-			items.AddRange(await query.Skip(fromIndex).Take(takeCount).ToArrayAsync());
+		if (items.Count() < pageSize) {
+			var takeCount = pageSize - items.Count();
+			var skipCount = Math.Max(0, offset - leftPaddingItems.Length);
+			items.AddRange(await query.Skip(skipCount).Take(takeCount).ToArrayAsync());
 		}
 
 		// This calculation is faster than `Math.Ceiling(rowCount / pageSize)`
